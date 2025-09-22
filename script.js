08e5bfcf45881f0c854b21b83b9ebfdf3f4fe76c
masjidalbarokah <masjidalbarokah0609@gmail.com>
2025-09-22 13:20:19 +0700

    Update script.js

--- script.js
+++ script.js
@@ -35,12 +35,3 @@ document.addEventListener("DOMContentLoaded", () => {
   const progress = document.querySelector(".progress-bar");
   progress.style.width = "60%"; // bisa ganti dari database
 });
-
-// Bisa dikembangin untuk ambil jadwal sholat otomatis dari API
-console.log("Landingpage Masjid ready!");
-
-// Animasi progress bar donasi
-document.addEventListener("DOMContentLoaded", () => {
-  const progress = document.querySelector(".progress-bar");
-  progress.style.width = "60%"; // bisa ganti dari database
-});
